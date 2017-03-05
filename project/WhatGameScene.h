100f52315b813f12adcfc5004cd8c0b54d462d23
xposimon <472032034@qq.com>
2017-03-05 18:09:55 +0800

    Add files via upload

---
+++ project/WhatGameScene.h
@@ -0,0 +1,22 @@
+#ifndef __WHATGAME_SCENE_H__
+#define __WHATGAME_SCENE_H__
+
+#include "cocos2d.h"
+
+
+
+class WhatGame : public cocos2d::Layer
+{
+public:
+    static cocos2d::Scene* createScene();
+
+    virtual bool init();
+    
+    // a selector callback
+    void menuCloseCallback(cocos2d::Ref* pSender);
+	
+    // implement the "static create()" method manually
+    CREATE_FUNC(WhatGame);
+};
+
+#endif // __WhatGame_SCENE_H__
