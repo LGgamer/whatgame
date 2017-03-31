d4959165e1b5cd4581ff8da03a0e30a4ebb52181
21Gunses <easonyzc96@gmail.com>
2017-03-31 16:08:46 +0800

    Update character.h

--- Character/character.h
+++ Character/character.h
@@ -5,45 +5,38 @@
 
 void debug();
 
-class player{
+class character{
 
 public:
-    player();
-    ~player();
+    character();
+    ~character();
     int get_health();
     int get_attack();
     int get_defence();
     int get_strength();
     int get_agility();
     int get_experience();
-    int get_coins();
-    int level_up();
+    int get_gold();
+    //int level_up();
     int get_speed();
 
-    int health;
-    int attack, defence;
-    int strength, agility;
-    int experience,coins, level;
-    int speed;
+
     //int shape_size;
     //virtual void move_way();
     std::string texture_path;
     double attack_coefficient, defence_coefficient, strength_coefficient, agility_coefficient;
-    double experience_coefficient, coins_coefficient;
+    double experience_coefficient, coins_coefficient, speed_coefficient;
+    double health_coefficient, gold_coefficient;
 
 private:
     int basic_health;
     int basic_attack;
     int basic_defence;
-    int basic strength;
+    int basic_strength;
     int basic_agility;
-    int basic_experience;
-    int basic_gold;
-    int basic_experience;
-    int basic_level;
+    int experience;
+    int gold;
+    int level;
     int basic_speed;
 
 };
-
-
-#endif // CHARACTER_H
