46c206cdbbf4c3242717ef8823095d8696a01e23
purified96 <illuminate1996@icloud.com>
2017-05-12 22:02:31 +0800

    Add files via upload

---
+++ dd/5.12 remove comments/PlayerLayer.cpp
@@ -0,0 +1,20 @@
+#include "PlayerLayer.h"
+
+USING_NS_CC;
+
+
+bool PlayerLayer::init() {
+	if (!Layer::init())
+		return false;
+	
+	//first sprite in game start page
+	auto screenSize = Director::getInstance()->getVisibleSize();
+	auto sprite = Sprite::create("HelloWorld.png");
+	sprite->setPosition(Vec2(40, screenSize.height / 2));
+	sprite->setScale(0.3);
+	this->addChild(sprite);
+	sprite->setTag(10);
+
+
+	return true;
+}
---
+++ dd/5.12 remove comments/PlayerLayer.h
@@ -0,0 +1,17 @@
+#ifndef _PLAYERLAYER_H_
+#define _PLAYERLAYER_H_
+
+#include "cocos2d.h"
+USING_NS_CC;
+
+class PlayerLayer : public Layer
+{
+public:
+
+	virtual bool init();
+
+
+	CREATE_FUNC(PlayerLayer);
+};
+
+#endif
