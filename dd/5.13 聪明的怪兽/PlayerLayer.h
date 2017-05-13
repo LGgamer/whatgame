581fdfd914636731b1b732934315d849951a5874
DryDew <479914232@qq.com>
2017-05-13 19:56:56 +0800

    1

---
+++ dd/5.13 聪明的怪兽/PlayerLayer.h
@@ -0,0 +1,16 @@
+#ifndef _PLAYERLAYER_H_
+#define _PLAYERLAYER_H_
+
+#include "cocos2d.h"
+USING_NS_CC;
+
+class PlayerLayer : public Layer
+{
+public:
+
+	virtual bool init();
+
+	CREATE_FUNC(PlayerLayer);
+};
+
+#endif
